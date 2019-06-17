f2708fc6d985777ec4dc7603caa115c422d5847b
FEUILLASTRE Donnovan <donnovan42320@gmail.com>
2019-06-17 20:44:37 +0200

    correct redirection

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -39,7 +39,7 @@ $app->setService('render', function (string $template, Array $params = [])
     }
 
     ob_start();
-    include __DIR__ . '/../View/' . $template . '.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/View/' . $template . '.php';
     ob_end_flush();
     die();
 });
