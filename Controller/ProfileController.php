3dc9934fafa87d1cf7bfddf00e7e97af25091c23
FEUILLASTRE Donnovan <donnovan42320@gmail.com>
2019-06-18 17:25:35 +0200

    ddd

--- Controller/ProfileController.php
+++ Controller/ProfileController.php
@@ -99,9 +99,9 @@ class ProfileController extends ControllerBase
 
             $profile = new ProfileGateway($this->app);
             $profile->Hydrate($element);
-            var_dump($profile);
+            //var_dump($profile);
             $result = $this->app->getService('profileFinder')->UserSearch($profile->GetLogin());
-            var_dump($result);
+            //var_dump($result);
         } catch (\Exception $e) {
             $render = $this->app->getService('render');
             $render('Search',['error' => $e, 'profile' => $profile, 'search' => $result]); // On renvoie la profile acutelle au template
